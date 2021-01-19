835303da7adbe74c1b6b7fef92f00295657f4ff4
dafa ilyasa <dafailyasa30@gmail.com>
2021-01-19 17:18:13 +0700

    login o auth callback google

---
+++ src/google.strategy.ts
@@ -0,0 +1,28 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, VerifyCallback } from "passport-google-oauth20";
+import { config } from "dotenv";
+
+import { Injectable } from "@nestjs/common";
+
+config()
+
+@Injectable()
+
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: '444831648451-i1ff4uj23sosg0pm26hbdgu6hsp728ua.apps.googleusercontent.com',
+      clientSecret: 'GjDd4UM9lNqqgObyMHsYlb7K',
+      callbackURL: 'http://localhost:3000/google/redirect',
+      scope: ['email', 'profile']
+    })
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    const { name, emails, photo } = profile;
+    const user = {
+      name, emails, photo
+    }
+    done(null, user)
+  }
+}
