3d6ec3f85ba48ab77b0cfc0dee6bc421f65fc9b9
dafailyasa <dafailyasa30@gmail.com>
2021-02-01 16:25:21 +0700

    write code be better

--- src/app.service.ts
+++ src/app.service.ts
@@ -39,24 +39,25 @@ export class AppService {
     };
   }
   async createRepository(req): Promise<Observable<AxiosResponse<any>>> {
-    if (!req.headers.authorization) {
+    if (!req.headers.authorization ) {
       throw new HttpException(
         "please return the token!",
         HttpStatus.BAD_REQUEST
       );
     }
 
+    const data = req.body.name
+    const reqHeader = {headers: {
+      "Content-Type": "application/json",
+      Authorization: req.headers.authorization,
+      Accept: "application/vnd.github.nebula-preview+json",
+    }}
+    
     return await this.httpService
       .post(
         this.DATA_URL,
-        { name: req.body.name },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: req.headers.authorization,
-            Accept: "application/vnd.github.nebula-preview+json",
-          },
-        }
+        { name: data},
+        reqHeader
       )
       .pipe(
         map((res) => {
