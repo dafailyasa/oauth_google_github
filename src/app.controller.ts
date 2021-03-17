d8ab65bf3589b7b30d2480c535e97d073d934b6d
dafailyasa <dafailyasa30@gmail.com>
2021-03-17 15:47:38 +0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { AuthGuard } from "@nestjs/passport";
+import { CreateRepository } from './github.dto'
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   // @Get()
   // getHello(): string {
@@ -12,7 +13,7 @@ export class AppController {
 
   @Get("google")
   @UseGuards(AuthGuard("google"))
-  async googleAuth(@Req() req) {}
+  async googleAuth(@Req() req) { }
 
   @Get("google/redirect")
   @UseGuards(AuthGuard("google"))
@@ -22,16 +23,16 @@ export class AppController {
 
   @Get("github")
   @UseGuards(AuthGuard("github"))
-  async githubAuth(@Req() req) {}
+  async githubAuth(@Req() req) { }
 
   @Get("github/redirect")
   @UseGuards(AuthGuard("github"))
   GithubAuthRedirect(@Req() req) {
     return this.appService.GithubLogin(req);
   }
-  
+
   @Post("github/create/repository")
-  async CreateRepository(@Req() req) {
-    return await this.appService.createRepository(req);
+  async CreateRepository(@Req() createRepository : CreateRepository) {
+    return await this.appService.createRepository(createRepository);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,66 +3,96 @@ import {
   HttpService,
   HttpException,
   HttpStatus,
-} from "@nestjs/common";
-import { map } from "rxjs/operators";
-import { AxiosResponse } from "axios";
-import { Observable } from "rxjs";
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { CreateRepository, RepositoryResponseObject } from './github.dto';
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class AppService {
-  private readonly DATA_URL = "https://api.github.com/user/repos";
+  private readonly DATA_URL = 'https://api.github.com/user/repos';
 
   constructor(private httpService: HttpService) {}
 
   getHello(): string {
-    return "Hello World!";
+    return 'Hello World!';
   }
 
   googleLogin(req) {
     if (!req.user) {
-      return "No user from google";
+      return 'No user from google';
     }
+    const source: {
+      userAgent: req.headers['user-agent'];
+    };
+    console.log(`Receiving timeline from source: ${source}`);
 
     return {
-      message: "User information from google",
+      message: 'User information from google',
       user: req.user,
     };
   }
   GithubLogin(req) {
     if (!req.user) {
-      return "No user from Github";
+      return 'No user from Github';
     }
 
     return {
-      message: "User information from Github",
+      message: 'User information from Github',
       user: req.user,
     };
   }
-  async createRepository(req): Promise<Observable<AxiosResponse<any>>> {
-    if (!req.headers.authorization ) {
-      throw new HttpException(
-        "please return the token!",
-        HttpStatus.BAD_REQUEST
-      );
+  async createRepository(req): Promise<RepositoryResponseObject[]> {
+    if (!req.headers.authorization) {
+      throw new HttpException('input the token', HttpStatus.BAD_REQUEST);
     }
 
-    const data = req.body.name
-    const reqHeader = {headers: {
-      "Content-Type": "application/json",
-      Authorization: req.headers.authorization,
-      Accept: "application/vnd.github.nebula-preview+json",
-    }}
-    
-    return await this.httpService
-      .post(
-        this.DATA_URL,
-        { name: data},
-        reqHeader
-      )
-      .pipe(
-        map((res) => {
-          return res.data;
-        })
+    const name = { name: req.body.name };
+    const token = req.headers.authorization;
+    try {
+      const res = await axios.post(
+        'https://api.github.com/user/repos',
+        JSON.stringify(name),
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/vnd.github.nebula-preview+json',
+          },
+        },
       );
+      return res.data;
+    } catch (error) {
+      return error;
+    }
   }
+
+  // async createRepository(req): Promise<Observable<AxiosResponse<any>>> {
+  //   if (!req.headers.authorization) {
+  //     throw new HttpException(
+  //       "please return the token!",
+  //       HttpStatus.BAD_REQUEST
+  //     );
+  //   }
+
+  //   const data = req.body.name
+  //   const reqHeader = {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: req.headers.authorization,
+  //       Accept: "application/vnd.github.nebula-preview+json",
+  //     }
+  //   }
+  //   return await this.httpService
+  //     .post(
+  //       this.DATA_URL,
+  //       { name: data },
+  //       reqHeader
+  //     )
+  //     .pipe(
+  //       map((res) => {
+  //         return res.data;
+  //       })
+  //     );
+  // }
 }
